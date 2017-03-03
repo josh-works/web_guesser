fa3f35b9054229604528abc18a4f7d750e1bd50d
Josh Thompson <thompsonjoshd@gmail.com>
2017-03-03 16:26:37 -0700

    take and respond to guesses

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,22 +1,38 @@
+require 'pry'
 require 'sinatra'
 require 'sinatra/reloader'
 
-number = rand(0..100)
-response = {
-  too_high: "too high",
-  way_too_high: "way too high",
-  too_low: "too low",
-  way_too_low: "way too low"
-}
+
+class WebGuesser
+  attr_reader :number, :response
+
+  def initialize
+    @number = rand(0..100)
+    @response = {
+      too_high: "too high",
+      way_too_high: "way too high",
+      too_low: "too low",
+      way_too_low: "way too low",
+      correct: "correct!"
+    }
+  end
 
 
+  def build_response(guess)
+    return response[:too_high] if guess > number
+    return response[:too_low] if guess < number
+    return response[:correct] if guess == number
+  end
+end
+
+web_guesser = WebGuesser.new
 
 get '/' do
   # throw params["guess"]
-  guess = params['guess']
+  guess = params['guess'].to_i
   erb :index, :locals => {
-        :number => number,
-        :response => response,
+        :number => web_guesser.number,
+        :message => web_guesser.build_response(guess),
         :guess => guess
       }
 end
