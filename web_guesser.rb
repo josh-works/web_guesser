09ac337fd3b920f0b4aaa79adae7a8f9e03b71b5
Josh Thompson <thompsonjoshd@gmail.com>
2017-03-03 15:48:23 -0700

    guess printed to page, as well as hash of possible responses.

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,9 +2,23 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 number = rand(0..100)
+response = {
+  too_high: "too high",
+  way_too_high: "way too high",
+  too_low: "too low",
+  way_too_low: "way too low"
+}
+
+
 
 get '/' do
-  erb :index, :locals => {:number => number}
+  # throw params["guess"]
+  guess = params['guess']
+  erb :index, :locals => {
+        :number => number,
+        :response => response,
+        :guess => guess
+      }
 end
 
 get '/datetime' do
