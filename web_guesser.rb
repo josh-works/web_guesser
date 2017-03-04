fcf174f3a52655ee75d9dd2eda7070cb0d79e0ea
Josh Thompson <thompsonjoshd@gmail.com>
2017-03-03 20:41:58 -0700

    add some bgcolors

--- web_guesser.rb
+++ web_guesser.rb
@@ -8,30 +8,53 @@ class WebGuesser
 
   def initialize
     @number = rand(0..100)
-    @response = {
-      too_high: "too high",
-      way_too_high: "way too high",
-      too_low: "too low",
-      way_too_low: "way too low",
-      correct: "correct! the number is #{number}"
-    }
   end
 
-  def build_response(guess)
-    return response[:way_too_high] if (guess - number ) > 5
-    return response[:too_high] if guess > number
-    return response[:way_too_low] if (guess - number ) < -5
-    return response[:too_low] if guess < number
-    return response[:correct] if guess == number
+  RESPONSE = {
+    too_high: "too high",
+    way_too_high: "way too high",
+    too_low: "too low",
+    way_too_low: "way too low",
+    correct: "correct! the number is #{@number}",
+    waiting: "Please enter a guess"
+  }
+
+  BGCOLORS = {
+  way_too_high: "#DC143C",
+  too_high: "#CD5C5C",
+  way_too_low: "#00008B",
+  too_low: "#00FFFF",
+  correct: "#97c475",
+  }
+
+
+  def build_response(guess = nil )
+    return RESPONSE[:waiting] if guess.nil?
+    return RESPONSE[:way_too_high] if (guess - number ) > 5
+    return RESPONSE[:too_high] if guess > number
+    return RESPONSE[:way_too_low] if (guess - number ) < -5
+    return RESPONSE[:too_low] if guess < number
+    return RESPONSE[:correct] if guess == number
+  end
+
+  def background_color(guess)
+    return BGCOLORS[:way_too_high] if (guess - number ) > 5
+    return BGCOLORS[:too_high] if guess > number
+    return BGCOLORS[:way_too_low] if (guess - number ) < -5
+    return BGCOLORS[:too_low] if guess < number
+    return BGCOLORS[:correct] if guess == number
   end
 end
+
+
 
 web_guesser = WebGuesser.new
 
 get '/' do
   guess = params['guess'].to_i
   erb :index, :locals => {
-        :message => web_guesser.build_response(guess)
+        :message => web_guesser.build_response(guess),
+        :background => web_guesser.background_color(guess)
       }
 end
 
